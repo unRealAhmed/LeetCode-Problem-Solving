f16e428c7321d519dce5a05ad40bf7b693b4bc04
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-02 12:04:37 +0200

    solved

---
+++ Return Length of Arguments Passed.ts
@@ -0,0 +1,5 @@
+export type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    return args.length
+};
