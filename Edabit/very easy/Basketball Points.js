a7b066e7b4db5a6c244a0ae74870ac1913d19e16
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:16:26 +0200

    solved

---
+++ Edabit/very easy/Basketball Points.js
@@ -0,0 +1,11 @@
+function points(twoPointers, threePointers) {
+    return (twoPointers * 2) + (threePointers * 3)
+}
+
+const result1 = points(7, 5);
+const result2 = points(10, 4);
+const result3 = points(38, 5);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
