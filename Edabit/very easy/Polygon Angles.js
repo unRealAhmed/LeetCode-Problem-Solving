5d811be6e748984fb191acedf40a4e35fb9d75d1
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:15:49 +0200

    solved

---
+++ Edabit/very easy/Polygon Angles.js
@@ -0,0 +1,11 @@
+function sumPolygon(n) {
+	return (n - 2) * 180
+}
+
+const result1 = sumPolygon(3);
+const result2 = sumPolygon(6);
+const result3 = sumPolygon(4);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
