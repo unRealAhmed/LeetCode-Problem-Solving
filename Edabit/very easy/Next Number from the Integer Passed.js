abb670630cca947f04927681a4a401c1c0394477
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:16:04 +0200

    solved

---
+++ Edabit/very easy/Next Number from the Integer Passed.js
@@ -0,0 +1,11 @@
+function addition(num) {
+	return num + 1
+}
+
+const result1 = addition(0);
+const result2 = addition(9);
+const result3 = addition(-3);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
