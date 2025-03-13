0bb4613bf98d9da7139e3760244ac720a17d3f54
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:16:21 +0200

    solved

---
+++ Edabit/very easy/Buggy Code (Part 1).js
@@ -0,0 +1,11 @@
+function cubes(a) {
+	return a ** 3
+}
+
+const result1 = cubes(7);
+const result2 = cubes(10);
+const result3 = cubes(38);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
