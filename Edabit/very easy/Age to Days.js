0ba5fd203a174c5f9ecd6e92aa7b345975f668e7
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:16:37 +0200

    solved

---
+++ Edabit/very easy/Age to Days.js
@@ -0,0 +1,11 @@
+function calcAge(age) {
+	return age * 365
+}
+
+const result1 = calcAge(8);
+const result2 = calcAge(10);
+const result3 = calcAge(7);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
