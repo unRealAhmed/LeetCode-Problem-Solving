9e8fc2743a5248cce311890f57122f25a46fd630
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:15:53 +0200

    solved

---
+++ Edabit/very easy/Perimeter of a Rectangle.js
@@ -0,0 +1,11 @@
+function findPerimeter(length, width) {
+    return (length + width) * 2
+}
+
+const result1 = findPerimeter(3, 4);
+const result2 = findPerimeter(6, 7);
+const result3 = findPerimeter(4, 9);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
