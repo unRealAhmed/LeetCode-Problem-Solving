4654dbedbe94fca5aeea7f3c4f1e2c0f77771a63
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:16:09 +0200

    solved

---
+++ Edabit/very easy/Less than or Equal to Zero.js
@@ -0,0 +1,11 @@
+function lessThanOrEqualToZero(num) {
+	return num <= 0
+}
+
+const result1 = lessThanOrEqualToZero(8); // false
+const result2 = lessThanOrEqualToZero(-10); // true
+const result3 = lessThanOrEqualToZero(24); // false
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
