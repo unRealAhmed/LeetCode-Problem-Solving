1dbc1bb2864c13a5799572dad3bc19ac5367f15d
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:16:13 +0200

    solved

---
+++ Edabit/very easy/Hours into Seconds.js
@@ -0,0 +1,11 @@
+function howManySeconds(hours) {
+	return hours * 60 * 60
+}
+
+const result1 = howManySeconds(8);
+const result2 = howManySeconds(10);
+const result3 = howManySeconds(24);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
