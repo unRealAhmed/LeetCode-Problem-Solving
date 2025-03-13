5836b6502951d9556bc9c36959c1f70dea1708a2
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:16:16 +0200

    solved

---
+++ Edabit/very easy/Correct the Mistakes.js
@@ -0,0 +1,11 @@
+function squared(b) {
+	return b * b
+}
+
+const result1 = squared(8);
+const result2 = squared(10);
+const result3 = squared(24);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
