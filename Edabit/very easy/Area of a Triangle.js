19942d96cb22866bb0c1b6838cbaef9de7b4501c
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:16:29 +0200

    solved

---
+++ Edabit/very easy/Area of a Triangle.js
@@ -0,0 +1,11 @@
+function triArea(base, height) {
+	return (base * height) / 2
+}
+
+const result1 = triArea(8, 2);
+const result2 = triArea(10, 6);
+const result3 = triArea(7, 4);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
