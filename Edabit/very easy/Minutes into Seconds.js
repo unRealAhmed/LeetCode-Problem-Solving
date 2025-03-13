ede1c4bb91752f5e2584fc76f9942896e80f8d8c
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:15:36 +0200

    solved

---
+++ Edabit/very easy/Minutes into Seconds.js
@@ -0,0 +1,11 @@
+function convert(minutes) {
+	return minutes * 60
+}
+
+const result1 = convert(5);
+const result2 = convert(3);
+const result3 = convert(2);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
