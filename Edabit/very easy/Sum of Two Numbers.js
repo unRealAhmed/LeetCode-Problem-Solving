a90718444aa6a101338dce7ac23084198e0a4538
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:15:06 +0200

    start edabit challenges

---
+++ Edabit/very easy/Sum of Two Numbers.js
@@ -0,0 +1,11 @@
+function addition(a, b) {
+    return a + b;
+}
+
+const result1 = addition(3, 2);
+const result2 = addition(-3, -6);
+const result3 = addition(7, 3);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
