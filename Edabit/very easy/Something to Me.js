88b3d39c248c6cc3e14ee4b627abdab9d72e7afa
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-13 20:15:44 +0200

    solved

---
+++ Edabit/very easy/Something to Me.js
@@ -0,0 +1,11 @@
+function giveMeSomething(a) {
+	return `something ${a}`
+}
+
+const result1 = giveMeSomething("is better than nothing");
+const result2 = giveMeSomething("Bob Jane");
+const result3 = giveMeSomething("something");
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
