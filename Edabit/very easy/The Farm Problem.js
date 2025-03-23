a4054b2eb4c46656265958587741fc98a6fd4965
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-23 21:45:06 +0200

    solved

---
+++ Edabit/very easy/The Farm Problem.js
@@ -0,0 +1,11 @@
+function animals(chickens, cows, pigs) {
+    return (chickens * 2) + (cows * 4) + (pigs * 4)
+}
+
+const result1 = animals(2, 3, 5)
+const result2 = animals(1, 2, 3)
+const result3 = animals(5, 2, 8)
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
