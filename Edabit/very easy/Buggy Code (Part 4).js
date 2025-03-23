d15d09735f8348151129c982fd1970c955718241
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-23 21:45:27 +0200

    solved

---
+++ Edabit/very easy/Buggy Code (Part 4).js
@@ -0,0 +1,16 @@
+function greeting(name) {
+	if (name == "Mubashir") {
+		return "Hello, my Love!";
+	} else {
+		return "Hello, " + name + "!";
+	}
+}
+
+
+const result1 = greeting("Matt")
+const result2 = greeting("Helen")
+const result3 = greeting("Mubashir")
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
