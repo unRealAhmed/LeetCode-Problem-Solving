fd25f7131fa8b01bb686b4405f4a937144d379e5
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-23 21:45:49 +0200

    solved

---
+++ Edabit/very easy/Remainder from Two Numbers.js
@@ -0,0 +1,14 @@
+function remainder(x, y) {
+    return x % y
+}
+
+
+const result1 = remainder(1, 3);
+const result2 = remainder(3, 4);
+const result3 = remainder(-9, 45);
+const result4 = remainder(5, 5);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(result4)
