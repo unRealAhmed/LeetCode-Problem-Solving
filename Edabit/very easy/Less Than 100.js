ff66353d3f8115d93fecc0e4237b0132ed2d2336
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-23 21:45:37 +0200

    solved

---
+++ Edabit/very easy/Less Than 100.js
@@ -0,0 +1,12 @@
+function lessThan100(a, b) {
+	return (a + b) < 100
+}
+
+
+const result1 = lessThan100(22, 15);
+const result2 = lessThan100(83, 34);
+const result3 = lessThan100(3, 77);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
