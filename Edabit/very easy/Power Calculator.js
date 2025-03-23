5798c4319518e18a970f28665a27410c2e088877
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-23 21:45:45 +0200

    solved

---
+++ Edabit/very easy/Power Calculator.js
@@ -0,0 +1,11 @@
+function circuitPower(voltage, current) {
+	return voltage * current
+}
+
+const result1 = circuitPower(8 , 10);
+const result2 = circuitPower(110, 3);
+const result3 = circuitPower(480, 20);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
