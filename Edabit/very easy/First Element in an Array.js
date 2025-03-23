67738d05a6ec8e5bd60ccdd2d5cb7eb7f1097a2a
ahmed sayed <ahmed.sayed.connect@gmail.com>
2025-03-23 21:45:15 +0200

    solved

---
+++ Edabit/very easy/First Element in an Array.js
@@ -0,0 +1,11 @@
+function getFirstValue(arr) {
+	return arr[0]
+}
+
+const result1 = getFirstValue([8 , 9]);
+const result2 = getFirstValue([10 , 8 , 9]);
+const result3 = getFirstValue([18 , 8 , 9]);
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
